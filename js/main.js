8d7abb5e322e75719314f9f6eb7fe3c96220bcff
Vladimir <vladimirstyopin@mail.ru>
2019-06-17 15:04:15 +0300

    Исправляет косяки

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 'use strict';
 
-var MAP_WIDTH = 1200;
 var MAP_HEIGHT_MAX = 630;
 var MAP_HEIGHT_MIN = 130;
 var NUMBER_OF_PINS = 8;
+var HOUSE_TYPE = ['palace', 'flat', 'house', 'bungalo'];
+var PIN_WIDTH = 50;
+var PIN_HEIGHT = 70;
 
 var pinList = document.querySelector('.map__pins');
-var houseType = ['palace', 'flat', 'house', 'bungalo'];
 var pinTemlate = document.querySelector('#pin').content.querySelector('.map__pin');
 var documentFragment = document.createDocumentFragment();
+var mapWidth = pinList.offsetWidth;
+var pinsData = [];
 
 var getRandom = function (arr) {
   var randomItem = Math.floor(Math.random() * arr.length);
@@ -18,26 +21,37 @@ var getRandom = function (arr) {
 var getPinsData = function () {
   var pinsAround = [];
   for (var i = 0; i < NUMBER_OF_PINS; i++) {
-    var xPoint = Math.floor(Math.random() * MAP_WIDTH);
+    var xPoint = Math.floor(Math.random() * mapWidth);
     var yPoint = Math.floor(Math.random() * (MAP_HEIGHT_MAX - MAP_HEIGHT_MIN + 1) + MAP_HEIGHT_MIN);
-    pinsAround[i] = {'author': {'avatar': 'img/avatars/user0' + (i + 1) + '.png'},
-      'offer': {'type': getRandom(houseType)},
-      'location': {'x': xPoint, 'y': yPoint}
+    pinsAround[i] = {
+      'author': {
+        'avatar': 'img/avatars/user0' + (i + 1) + '.png'
+      },
+      'offer': {
+        'type': getRandom(HOUSE_TYPE)
+      },
+      'location': {
+        'x': xPoint,
+        'y': yPoint
+      }
     };
   }
   return pinsAround;
 };
 
-var getPins = function (a) {
+pinsData = getPinsData();
+
+var getPins = function (arr) {
   var mapPin = pinTemlate.cloneNode(true);
-  mapPin.style = 'left: ' + (getPinsData()[a].location.x - 25) + 'px; top: ' + (getPinsData()[0].location.y - 70) + 'px;';
-  mapPin.querySelector('img').src = getPinsData()[a].author.avatar;
-  mapPin.querySelector('img').alt = getPinsData()[a].offer.type;
+  var avatarImg = mapPin.querySelector('img');
+  mapPin.style = 'left: ' + (arr.location.x - PIN_WIDTH / 2) + 'px; top: ' + (arr.location.y - PIN_HEIGHT) + 'px;';
+  avatarImg.src = arr.author.avatar;
+  avatarImg.alt = arr.offer.type;
   return mapPin;
 };
 
 for (var i = 0; i < NUMBER_OF_PINS; i++) {
-  documentFragment.appendChild(getPins(i));
+  documentFragment.appendChild(getPins(pinsData[i]));
 }
 
 pinList.appendChild(documentFragment);
