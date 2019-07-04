e8facff783ad8bcd182a32b59a4289283169d360
Vladimir <vladimirstyopin@mail.ru>
2019-07-04 16:26:35 +0300

    исправляет статус ошибок сети

--- js/data.js
+++ js/data.js
@@ -5,19 +5,19 @@
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
     xhr.addEventListener('load', function () {
-      xhr.timeout = 1000;
       if (xhr.status === 200) {
         onLoad(xhr.response);
       } else {
-        onError(' статус ошибки ' + xhr.status + ' ' + xhr.statusText);
+        onError(', статус ошибки ' + xhr.status + ' ' + xhr.statusText);
       }
-      xhr.addEventListener('error', function () {
-        onError(' статус ошибки ' + xhr.status + ' ' + xhr.statusText);
-      });
-      xhr.addEventListener('timeout', function () {
-        onError(' запрос не успел выполниться за ' + xhr.timeout + ' мс');
-      });
     });
+    xhr.addEventListener('error', function () {
+      onError(' при подключении к серверу');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError(', запрос не успел выполниться за ' + xhr.timeout + ' мс');
+    });
+    xhr.timeout = 1000;
     xhr.open('GET', 'https://js.dump.academy/keksobooking/data');
     xhr.send();
   };
